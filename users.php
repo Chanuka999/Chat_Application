afaf1fa05113b431251dab1c8a9cc34e3dea5b62
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-30 23:13:28 +0530

    update

--- users.php
+++ users.php
@@ -1,61 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Real Time Chat App</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
+<?php
+ session_start();
+ if(!isset($_SESSION['unique_id'])){
+  header("location:login.php");
+ }
+?>
+
+<?php include "header.php"; ?>
   <body>
     <div class="wrapper">
       <section class="users">
         <header>
+          <?php
+             include "php/config.php";
+             $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+             if(mysqli_num_rows($sql) >0){
+              $row = mysqli_fetch_assoc($sql);
+             }
+          ?>
           <div class="content">
-            <img src="images/img1.jfif" alt="" />
+            <img src="images/<?php echo $row['img']; ?>" alt="" />
             <div class="details">
-              <span>coding </span>
-              <p>Active now</p>
+              <span><?php echo $row['fname'] . " " . $row['lname'];?></span>
+              <p><?php echo $row['status']; ?></p>
             </div>
           </div>
           <a href="#" class="logout">Logout</a>
         </header>
         <div class="search">
           <span class="text">Select an user to start chat</span>
-          <input type="text" placeholder="Ener name to search..." />
+          <input type="text" placeholder="Ener name to search...">
           <button><i class="fas fa-search"></i></button>
         </div>
         <div class="users-list">
-          <a href="">
-            <div class="content">
-              <img src="images/img1.jfif" alt="" />
-              <div class="details">
-                <span>Code</span>
-                <p>This is a text message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img src="images/img1.jfif" alt="" />
-              <div class="details">
-                <span>Code</span>
-                <p>This is a text message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img src="images/img1.jfif" alt="" />
-              <div class="details">
-                <span>Code</span>
-                <p>This is a text message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-        </div>
+          
+         
       </section>
     </div>
     <script src="javascript/users.js"></script>
