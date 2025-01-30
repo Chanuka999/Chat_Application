f467dc495b6a85db160fcd5adf55d52507392384
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-30 23:13:43 +0530

    add to javascript file

--- javascript/login.js
+++ javascript/login.js
@@ -10,17 +10,18 @@ continueBtn.onclick = () => {
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "php/login.php", true);
   xhr.onload = () => {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         let data = xhr.response;
         console.log(data);
 
-        // if (data == "success") {
-        // } else {
-        //   errorText.textContent = data;
-        //   0;
-        //   errorText.style.display = "block";
-        // }
+        if (data == "success") {
+          location.href = "users.php";
+        } else {
+          errorText.textContent = data;
+
+          errorText.style.display = "block";
+        }
       }
     }
   };
