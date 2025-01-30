78bb18f3a32ad815a786d309092837e5df243421
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-31 01:13:24 +0530

    update

--- javascript/users.js
+++ javascript/users.js
@@ -8,9 +8,32 @@ searchBtn.onclick = () => {
   searchBar.classList.toggle("active");
 };
 
+searchBar.onkeyup = () => {
+  let searchTerm = searchBar.value;
+  if (searchTerm != "") {
+    searchBar.classList.add("active");
+  } else {
+    searchBar.classList.remove("active");
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/search.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        if (!searchBar.classList.contains("active")) {
+          userList.innerHTML = data;
+        }
+      }
+    }
+  };
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("searchTerm=" + searchTerm);
+};
+
 setInterval(() => {
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "php/users.php", true);
+  xhr.open("GET", "php/users.php", true);
   xhr.onload = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
