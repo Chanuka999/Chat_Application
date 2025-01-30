d02c2e7040756baff74dbfb77e9795ccc7db3bd2
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-30 12:22:04 +0530

    update

--- javascript/pass-show-hide.js
+++ javascript/pass-show-hide.js
@@ -1,14 +1,18 @@
-const pswrdField = document.querySelector(
-    ".form .field input[type='password]'"
-  ),
-  toggleBtn = document.querySelector(".form .field i");
+document.addEventListener("DOMContentLoaded", function () {
+  const pswrdField = document.querySelector(
+    ".form .field input[type='password']"
+  );
+  const toggleBtn = document.querySelector(".form .field i");
 
-toggleBtn.onClick = () => {
-  if (pswrdField.type == "password") {
-    pswrdField.type = "text";
-    toggleBtn.classList.add("active");
-  } else {
-    pswrdField.type = "password";
-    toggleBtn.classList.remove("active");
+  if (pswrdField && toggleBtn) {
+    toggleBtn.onclick = () => {
+      if (pswrdField.type === "password") {
+        pswrdField.type = "text";
+        toggleBtn.classList.add("active");
+      } else {
+        pswrdField.type = "password";
+        toggleBtn.classList.remove("active");
+      }
+    };
   }
-};
+});
