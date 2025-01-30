9d01142e30e08ff81b7f08b869fd131bf8eaa92d
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-30 12:22:32 +0530

    update

--- javascript/users.js
+++ javascript/users.js
@@ -1,7 +1,7 @@
 const searchBar = document.querySelector(".users .search input"),
   searchBtn = document.querySelector(".users .search button");
 
-searchBtn.onClick = () => {
+searchBtn.onclick = () => {
   searchBar.classList.toggle("active");
   searchBar.focus();
   searchBar.classList.toggle("active");
