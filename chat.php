f05b1f72d6d2fcd6002316d4e6997cd9aa2babf7
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-31 10:45:41 +0530

    code

--- chat.php
+++ chat.php
@@ -12,15 +12,16 @@
         <header>
         <?php
              include "php/config.php";
-             $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+             $user_id = mysqli_real_escape_string($conn,$_GET['user_id']);
+             $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
              if(mysqli_num_rows($sql) >0){
               $row = mysqli_fetch_assoc($sql);
              }
           ?>
           <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-          <img src="images/img1.jfif" alt="" />
+          <img src="images/<?php echo $row['img']; ?>" alt="" />
           <div class="details">
-            <span>coding </span>
+            <span><?php echo $row['fname'] . " " . $row['lname'];?></span>
             <p>Active now</p>
           </div>
         </header>
@@ -59,11 +60,14 @@
             </div>
           </div>
         </div>
-        <form action="#" class="typing-area">
-          <input type="text" placeholder="Type a massege here...." />
+        <form action="#" class="typing-area" autocomplete="off">
+          <input type="text" name="outgoing_id" value="<?php echo $_SESSION['unique_id']; ?>" hidden>
+          <input type="text" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+          <input type="text" name="message" class="input-field" placeholder="Type a massege here...." />
           <button><i class="fab fa-telegram-plane"></i></button>
         </form>
       </section>
     </div>
+    <script src="javascript/chat.js"></script>
   </body>
 </html>
