fce035bddabb974a81ed17eaf6be9fa52534d1eb
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-31 14:14:26 +0530

    delete files

--- login.php
+++ login.php
@@ -1,31 +1,36 @@
-<?php include "header.php"; ?>
-  <body>
-    <div class="wrapper">
-      <section class="form login">
-        <header>Real Time Chat App</header>
-        <form action="#">
-          <div class="error-txt">This is an error message</div>
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
 
-          <div class="field input">
-            <label>Email Address</label>
-            <input type="email" name="email" placeholder="Email Address" required />
-          </div>
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <header>Realtime Chat App</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="field input">
+          <label>Email Address</label>
+          <input type="text" name="email" placeholder="Enter your email" required>
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter your password" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continue to Chat">
+        </div>
+      </form>
+      <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+    </section>
+  </div>
+  
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/login.js"></script>
 
-          <div class="field input">
-            <label>Password</label>
-            <input type="password" name="password" placeholder="Password" required />
-            <i class="fas fa-eye"></i>
-          </div>
-
-          <div class="field button">
-            <input type="submit" value="Continue to Chat" />
-          </div>
-        </form>
-
-        <div class="link">Not yet signed up? <a href="index.php">signup now</a></div>
-      </section>
-    </div>
-    <script src="javascript/pass-show-hide.js"></script>
-    <script src="javascript/login.js"></script>
-  </body>
+</body>
 </html>
